f26f4b06860c6162d2247080a5f2dbee92aa858f
[Agnes Font] <[afonttorres@gmail.com]>
2022-05-02 11:10:58 +0200

    git ignore

--- fizzbuzz/src/fizzbuzz.test.js
+++ fizzbuzz/src/fizzbuzz.test.js
@@ -2,5 +2,15 @@ const fizzbuzz = require('./fizzbuzz');
 
 test('should return Fizz when param is 3 div', ()=>{
     let result = fizzbuzz(6);
-    expect(result).toBe('Fizz')
+    expect(result).toBe('Fizz');
+})
+
+test('should return Buzz when param is 5 div', ()=>{
+    let result = fizzbuzz(10);
+    expect(result).toBe('Buzz');
+})
+
+test('should return FizzBuzz when param is 5 & 3 div', ()=>{
+    let result = fizzbuzz(30);
+    expect(result).toBe('FizzBuzz');
 })
