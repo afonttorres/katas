5ba609d5af27ff918b878bd4358351878c4b81c2
[Agnes Font] <[afonttorres@gmail.com]>
2022-05-02 13:34:27 +0200

    all tests working

--- fizzbuzz/src/fizzbuzz.js
+++ fizzbuzz/src/fizzbuzz.js
@@ -1,13 +1,21 @@
-const fizzbuzz = (num)=>{
-    if (num % 3 === 0 && num % 5 === 0){
+const fizzbuzz = (num) => {
+    if (num % 3 === 0 && num % 5 === 0) {
         return 'FizzBuzz'
-    }else if(num % 5 === 0){
+    } else if (num % 5 === 0) {
         return 'Buzz'
-    }else if (num % 3 === 0){
+    } else if (num % 3 === 0) {
         return 'Fizz'
-    }else{
+    } else {
         return num
     }
 }
 
-module.exports = fizzbuzz;
+const returnDiv = (arr, num) => {
+    return arr.filter(arrnum => arrnum % num === 0)
+}
+
+const returnAdults = (arr)=>{
+    return arr.filter(arruser => arruser.age >= 18)
+}
+
+module.exports = { fizzbuzz, returnDiv, returnAdults };
--- fizzbuzz/src/fizzbuzz.test.js
+++ fizzbuzz/src/fizzbuzz.test.js
@@ -1,21 +1,66 @@
-const fizzbuzz = require('./fizzbuzz');
+const { fizzbuzz, returnDiv, returnAdults } = require('./fizzbuzz');
 
-test('should return Fizz when param is 3 div', ()=>{
+test('should return Fizz when param is 3 div', () => {
     let result = fizzbuzz(6);
     expect(result).toBe('Fizz');
 })
 
-test('should return Buzz when param is 5 div', ()=>{
+test('should return Buzz when param is 5 div', () => {
     let result = fizzbuzz(10);
     expect(result).toBe('Buzz');
 })
 
-test('should return FizzBuzz when param is 5 & 3 div', ()=>{
+test('should return FizzBuzz when param is 5 & 3 div', () => {
     let result = fizzbuzz(30);
     expect(result).toBe('FizzBuzz');
 })
 
-test('should return param when param is not div by 3 & 5', ()=>{
+test('should return param when param is not div by 3 & 5', () => {
     let result = fizzbuzz(4);
     expect(result).toBe(4);
+})
+
+test('should return numbers of the array div by num', () => {
+    let result = returnDiv([3, 6, 9, 10], 3);
+    expect(result).toStrictEqual([3, 6, 9]);
+})
+
+test('should return array with adult users', () => {
+    const users = [
+        {
+            name: 'Agnès',
+            age: 27
+        },
+        {
+            name: 'Nil',
+            age: 23
+        },
+        {
+            name: 'Africa',
+            age: 0
+        },
+        {
+            name: 'Marina',
+            age: 28
+        }
+    ]
+
+    let usersOutput = [
+        {
+            name: 'Agnès',
+            age: 27
+        },
+        {
+            name: 'Nil',
+            age: 23
+        },
+        {
+            name: 'Marina',
+            age: 28
+        }
+    ]
+
+    let result = returnAdults(users);
+
+    expect(result).toStrictEqual(usersOutput);
 })
